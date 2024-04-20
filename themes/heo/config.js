c574474247c4823c06830d1f38b0ab0ea61aa8a1
Xiaoheizi2023 <121916532+Xiaoheizi2023@users.noreply.github.com>
2024-04-20 23:05:17 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://blog.bbbql.top' },
+    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com/article/how-to-config-notion-next' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -15,12 +15,13 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
+  HEO_HERO_TITLE_1: '喜好摆烂',
+  HEO_HERO_TITLE_2: '旅游与生活',
+  HEO_HERO_TITLE_3: 'blog.bbbql.top',
   HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_5: '冰冰冰淇淋轻松定制主题',
+  HEO_HERO_TITLE_LINK: 'https://blog.bbbql.top',
+
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
